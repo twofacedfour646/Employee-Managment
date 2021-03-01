28e2806ddaebbd21dd8b18461ae5c0875d9a471d
twofacedfour646 <mdgubler8@gmail.com>
2021-02-28 22:25:44 -0500

    Initial commit

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Flask/React App</h1>'
+    return '<h1>Employee App</h1>'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
